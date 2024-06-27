dacf53ed1ad05462ddb252ca7522c2126c324cfe
Husnain Aslam <husnain.aslam@brainxtech.com>
2024-06-27 16:24:50 +0500

    Adds the CSS

--- src/controllers/todo_controller.js
+++ src/controllers/todo_controller.js
@@ -1,21 +1,12 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["description", "show", "mainButton"];
+  static targets = ["description", "show", "mainButton", "span", "entity"];
 
   initialize() {
     this.tasks = [];
   }
-  connect() {
-    // console.log("in the connect");
-    // this.descriptionTarget.addEventListener("keypress", (event) => {
-    //   console.log("in the function");
-    //   if (event.key === "Enter") {
-    //     console.log(this.mainButtonTarget);
-    //     this.mainButtonTarget.click();
-    //   }
-    // });
-  }
+  connect() {}
 
   listenEvent(event) {
     if (event.key === "Enter") {
@@ -25,49 +16,63 @@ export default class extends Controller {
 
   addTask() {
     const taskHtml = `
-      <span data-action="click->todo#edit">${this.description}</span>
-      <button data-action="todo#completed" class="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm p-3 text-center me-2 mb-2">Completed</button>
-      <button data-action="todo#delete" class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm p-3 text-center me-2 mb-2">Delete</button>
-    `;
+      <span data-action="click->todo#edit" data-todo-target="span" class="pt-2" >${this.description}</span>
+      <div>
+      <button data-action="todo#completed" class="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm p-2 text-center me-2 mb-2 mt-1">Completed</button>
+      <button data-action="todo#delete" class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm p-2 text-center me-2 mb-2">Delete</button>
+      </div>
+      `;
     this.tasks.unshift(taskHtml);
     this.descriptionTarget.value = "";
     this.updateShowTarget();
   }
 
   delete(event) {
-    console.log(event.target.parentElement.value);
-    const index = event.target.parentElement.value;
+    const parent = event.target.closest("[data-todo-target]");
+    console.log(parent);
+    const index = parseInt(parent.getAttribute("index"), 10);
     console.log(index);
     this.tasks.splice(index, 1);
     this.updateShowTarget();
   }
 
   completed(event) {
-    const parent = event.target.parentElement;
-    const index = parent.value;
-
-    console.log("before parent");
-    console.log(parent);
+    // const parent = event.target.parentElement;
+    // const index = parent.value;
+    // const spanElement = parent.querySelector("span");
+    const parent = event.target.closest('[data-todo-target="entity"]');
+    const index = parseInt(parent.getAttribute("index"), 10);
+    console.log(index);
     const spanElement = parent.querySelector("span");
-    if (spanElement) {
-      spanElement.innerHTML = `<del>${spanElement.textContent}</del>`;
-    }
-    parent
-      .querySelectorAll("button")
-      .forEach((button) => (button.disabled = true));
-    console.log("before changed parent");
-    console.log(parent);
+    console.log(spanElement);
 
-    // Update the task HTML in the tasks array
-    this.tasks[index] = parent.outerHTML;
+    spanElement.innerHTML = `<del class="mt-2">${spanElement.textContent}</del>`;
+
+    const taskHtml = `
+      <del class="pt-2" >${spanElement.textContent}</del>
+      <div>
+        <button class="bg-gray-300  cursor-not-allowed opacity-50 font-medium rounded-lg text-sm p-2 text-center me-2 mb-2 mt-1" disabled>
+       Completed
+      </button>
+      <button class="bg-gray-300 rounded-md cursor-not-allowed opacity-50 font-medium rounded-lg text-sm p-2 text-center me-2 mb-2 mt-1" disabled>
+    Delete
+  </button>
+      </div>
+      `;
+
+    this.tasks[index] = taskHtml;
     this.updateShowTarget();
   }
   edit(event) {
-    const parent = event.target.parentElement;
-    const index = parent.value;
+    const parent = event.target.closest('[data-todo-target="entity"]');
+    console.log(parent);
+    console.log(event.currentTarget.innerText);
+
+    const index = parseInt(parent.getAttribute("index"), 10);
     const description = parent.querySelector("span").innerHTML;
+
     const edit_field = `<input type="text" value="${description}" id="edit-input" /> 
-    <button data-action="todo#update" id="edit-button">Update</button>`;
+    <button data-action="todo#update" id="edit-button" class="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium p-2 m-2 rounded text-sm">Update</button>`;
     this.tasks[index] = edit_field;
     this.mainButton.disabled = true;
     this.updateShowTarget();
@@ -84,14 +89,15 @@ export default class extends Controller {
   }
 
   update(event) {
-    console.log("updated is called");
     const updated_description = document.getElementById("edit-input").value;
-    console.log(updated_description);
-    const parent = event.target.parentElement;
-    const index = parent.value;
-    const updated_entry = `<span data-action="click->todo#edit">${updated_description}</span>
-    <button data-action="todo#delete" class="delete-button">Delete</button>
-    <button data-action="todo#completed" class="completed-button">Completed</button>
+    const parent = event.target.closest("[data-todo-target]");
+    console.log(parent);
+    const index = parseInt(parent.getAttribute("index"), 10);
+    const updated_entry = `<span data-action="click->todo#edit class="inline-block pt-2">${updated_description}</span>
+    <div>
+      <button data-action="todo#completed" class="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm p-2 text-center me-2 mb-2 mt-1">Completed</button>
+      <button data-action="todo#delete" class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm p-2 text-center me-2 mb-2">Delete</button>
+      </div>
     `;
     this.tasks[index] = updated_entry;
     this.mainButton.disabled = false;
@@ -102,7 +108,12 @@ export default class extends Controller {
     this.showTarget.innerHTML = "";
     this.tasks.forEach((element, index) => {
       const taskElement = document.createElement("div");
-      taskElement.value = index;
+      taskElement.setAttribute("index", index);
+      taskElement.classList.add("flex");
+      taskElement.classList.add("justify-between");
+      taskElement.classList.add("flex-wrap");
+      taskElement.classList.add("w-full");
+      taskElement.setAttribute("data-todo-target", "entity");
       taskElement.innerHTML = element;
       this.showTarget.appendChild(taskElement);
     });
